39206b296cf7c491dfb26cb2fca6c9e366e2dd95
Max Horbul <max.horbul@livingsocial.com>
2012-02-03 15:36:18 -0800

    use cloned cookbooks

--- solo.rb
+++ solo.rb
@@ -1,5 +1,5 @@
 
 file_cache_path "/var/chef-solo"
-cookbook_path "/var/chef-solo/cookbooks"
-json_attribs "https://raw.github.com/mhorbul/remote-directory-notification/master/node.json"
-recipe_url "https://nodeload.github.com/mhorbul/remote-directory-notification/tarball/master"
+cookbook_path File.expand_path(File.dirname(__FILE__) + "/cookbooks")
+json_attribs File.expand_path(File.dirname(__FILE__) + "/node.json")
+
