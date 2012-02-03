3d06f8324c696bbcc22eba000575847d77103ef0
Max Horbul <max.horbul@livingsocial.com>
2012-02-03 15:20:03 -0800

    add solo.rb

---
+++ solo.rb
@@ -0,0 +1,5 @@
+
+file_cache_path "/var/chef-solo"
+cookbook_path "/var/chef-solo/cookbooks"
+json_attribs "https://raw.github.com/mhorbul/remote-directory-notification/master/node.json"
+recipe_url "https://nodeload.github.com/mhorbul/remote-directory-notification/tarball/master"
